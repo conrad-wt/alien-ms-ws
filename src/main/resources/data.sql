4570149e00fa23d51dfb725c0ea8e2a9c7170712
conrad-wt <conrad.wt97@hotmail.com>
2024-10-16 21:57:06 +0200

    feat: add liquibase and separate local running to prod

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into alien (id, name, tech) values (1, 'Bob', 'Plingus');
-insert into alien (id, name, tech) values (2,  'Regorb', 'Glipnorp');
-insert into alien (id, name, tech) values (3, 'Chat', 'KittenCannon');
--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-
-
-create table alien(
-    id int primary key,
-    name varchar(50),
-    tech varchar(100)
-);
